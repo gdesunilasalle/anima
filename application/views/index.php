446b6b5a33057b48c4802dc65dfb346afe70162f
leonardomartelotte <leonardomartelotte@10.50.4.93>
2018-03-04 21:19:09 -0300

    Implementado modal bootstrap para login (layer de login) e alterada a mensagem da pagina inicial. Mantive a pagina extra de login ao clicar em ANIMA?! apenas para testes.

--- application/views/index.php
+++ application/views/index.php
@@ -32,6 +32,10 @@
       <div class="container">
 
         <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url('login'); ?>">Anima?!</a>
+        
+        
+        
+        
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
@@ -63,12 +67,40 @@
       <div class="container">
         <div class="intro-text">
           <div class="intro-lead-in">Seja muito bem vindo ao Anima!</div>
-          <div class="intro-heading text-uppercase">JÁ SABE COMO FUNCIONA?<br>
-          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#oquee">O que é?</a>
-        </div>
+          <div class="intro-heading text-uppercase">JÁ É CADASTRADO?<br>
+     
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#" data-toggle="modal" data-target="#login-modal">Login</a>
       </div>
+          
+          <!-- INICIO DO MODAL BOOTSTRAP - LAYER DE LOGIN -->
+          
+          <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true" style="display: none;">
+    	  <div class="modal-dialog">
+				<div class="loginmodal-container">
+                                        <br>
+                                        <br>
+					<h1>Acesso do usuário:</h1><br>
+				  <form>
+					<input type="text" name="user" placeholder="Apelido">
+                                        <br>
+                                        <br>
+					<input type="password" name="pass" placeholder="Senha">
+                                        <br>
+                                        <br>
+					<input type="submit" name="login" class="login loginmodal-submit" value="Entrar">
+				  </form>
+					
+				  <div class="login-help">
+					<a href="#">Esqueci minha senha</a>
+				  </div>
+				</div>
+			</div>
+		  </div>
+          
+          <!-- FIM DO MODAL BOOTSTRAP - LAYER DE LOGIN -->
+          
+        </div>
     </header>
-
     <!-- O que é? -->
     <section id="oquee">
       <div class="container">
