f0190fbc992b86d92f75610dbb1e8fb126182561
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-08 15:59:11 -0300

    Login e criptografia
    O login está funcionando com criptografia

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -16,33 +16,43 @@ class Usuarios extends CI_Controller {
                 $this->load->view('index/cadastro');
                 $this->load->view('index/desenvolvedores');
                 $this->load->view('index/contato');
-                $this->load->view('index/rodape'); 
+                $this->load->view('index/rodape');
 	}
-        
-        public function login(){
-            $this->load->library('form_validation');
-            $this->form_validation->set_rules('txt-user','Apelido','required|min_length[6]');
-            $this->form_validation->set_rules('txt-senha','Senha','required|min_length[6]');
-            if($this->form_validation->run() == FALSE){
-                $this->index();
-            }else{
-                $usuario=$this->input->post('txt-user');
-                $senha=$this->input->post('txt-senha');
-                $this->db->where('apelido',$usuario);
-                $this->db->where('senha',$senha); 
-                $userlogado = $this->db->get('cadastrousuario')->result();
-                if(count($userlogado)==1){
-                    $dadosSessao['userlogado'] = $userlogado[0];
-                    $dadosSessao['logado'] = TRUE;
-                    $this->session->set_userdata($dadosSessao);
-                    redirect(base_url('anima'));
-                }else{
-                    $dadosSessao['userlogado'] = NULL;
-                    $dadosSessao['logado'] = FALSE;
-                    $this->session->set_userdata($dadosSessao);
-                    redirect(base_url('indice'));
 
-                }
-            }
-        }
+  public function login()
+{
+    $this->load->library("form_validation");
+
+
+    $this->form_validation->set_rules('txt-user','Apelido','required|min_length[6]');
+    $this->form_validation->set_rules('txt-senha','Senha','required|min_length[6]');
+
+
+        if (!$this->form_validation->run()) throw new UnexpectedValueException(validation_errors());
+
+        $user = $this->input->post('txt-user');
+        $password = $this->input->post('txt-senha');
+        $query = $this->db
+            ->select("*")
+            ->from("cadastrousuario")
+            ->where("apelido", $user)
+            ->get();
+        if ($query->num_rows() != 1)    echo"Usuário Incorreta!"; // User Incorreto
+        $row = $query->row();
+        if (!password_verify($password, $row->senha))
+{
+ // Senha Incorreta
+ $dadosSessao['userlogado'] = NULL;
+ $dadosSessao['logado'] = FALSE;
+ $this->session->set_userdata($dadosSessao);
+ redirect(base_url('indice'));
+}
+else{
+          // Logado com sucesso
+          $dadosSessao['userlogado'] = $userlogado[0];
+          $dadosSessao['logado'] = TRUE;
+          $this->session->set_userdata($dadosSessao);
+          redirect(base_url('anima'));
+}
+    }
 }
