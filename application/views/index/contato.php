baeffb629ccbda55d47225f4cdab7672b753470a
Lucas José <32078930+lucasjose10@users.noreply.github.com>
2018-03-08 14:33:27 -0300

    alterei o formulario de contato

--- application/views/index/contato.php
+++ application/views/index/contato.php
@@ -2,38 +2,38 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Contato Us</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h2 class="section-heading text-uppercase">Envie suas dúvidas, críticas ou sugestões por e-mail.</h2>
+            <h3 class="section-subheading text-muted">Entraremos em contato mais rápido possível</h3>
           </div>
         </div>
         <div class="row">
           <div class="col-lg-12">
-            <form id="contatoForm" name="sentMessage" novalidate>
+            <form id="contatoForm" name="envie sua mensagem" novalidate>
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Your Name *" required data-validation-required-message="Please enter your name.">
+                    <input class="form-control" id="name" type="text" placeholder="Seu nome *" required data-validation-required-message="Entre com seu nome.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="email" type="email" placeholder="Your Email *" required data-validation-required-message="Please enter your email address.">
+                    <input class="form-control" id="email" type="email" placeholder="Seu Email *" required data-validation-required-message="Entre com seu endereço de email.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required data-validation-required-message="Please enter your phone number.">
+                    <input class="form-control" id="phone" type="tel" placeholder="Seu Telefone *" required data-validation-required-message="Entre com seu telefone.">
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-group">
-                    <textarea class="form-control" id="message" placeholder="Your Message *" required data-validation-required-message="Please enter a message."></textarea>
+                    <textarea class="form-control" id="message" placeholder="Sua mensagem *" required data-validation-required-message="Entre com a mensagem."></textarea>
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="clearfix"></div>
                 <div class="col-lg-12 text-center">
                   <div id="success"></div>
-                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Enviar</button>
                 </div>
               </div>
             </form>
