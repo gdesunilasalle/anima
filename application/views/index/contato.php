cead90d0724b2261da0ad588ea8b6226ce063093
Guilherme Alexandre <ga.sousa07@gmail.com>
2018-03-09 17:00:31 -0300

    Textos editados ( O que? Contato e Desenvolvedores)

--- application/views/index/contato.php
+++ application/views/index/contato.php
@@ -12,22 +12,22 @@
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Seu nome *" required data-validation-required-message="Entre com seu nome.">
+                    <input class="form-control" id="name" type="text" placeholder="Nome *" required data-validation-required-message="Entre com seu nome.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="email" type="email" placeholder="Seu Email *" required data-validation-required-message="Entre com seu endereço de email.">
+                    <input class="form-control" id="email" type="email" placeholder="E-mail *" required data-validation-required-message="Entre com seu endereço de email.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="phone" type="tel" placeholder="Seu Telefone *" required data-validation-required-message="Entre com seu telefone.">
+                    <input class="form-control" id="phone" type="tel" placeholder="Telefone *" required data-validation-required-message="Entre com seu telefone.">
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-group">
 
-                    <textarea class="form-control" id="message" style="margin-top: 0px; margin-bottom: 0px; height: 146px;" placeholder="Sua mensagem *" required data-validation-required-message="Entre com a mensagem."></textarea>
+                    <textarea class="form-control" id="message" style="margin-top: 0px; margin-bottom: 0px; height: 146px;" placeholder="Mensagem *" required data-validation-required-message="Entre com a mensagem."></textarea>
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
