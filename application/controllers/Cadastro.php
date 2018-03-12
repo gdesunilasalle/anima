53ea9c1cff93f8193ad2e26f6c8cf4c7b6e1a6f4
Leonardo Martelotte <leonardomartelotte@gmail.com>
2018-03-12 12:39:07 -0300

    Validação de email Lasalle e melhorias gerais
    Agora o formulário não é apagado quando da erro, ele retorna com os ultimos dados digitados pelo usuario.

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -1,9 +1,12 @@
 <?php
 class Cadastro extends CI_Controller {
-
 function __construct() {
 parent::__construct();
 $this->load->model('insert_model');
+}
+public function email_check($email) 
+{
+  return strpos($email, '@soulasalle.com.br') || strpos($email, '@lasalle.org.br') !== false;
 }
 public function index() {
 //Including validation library
@@ -12,39 +15,36 @@ $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 //VALIDAÇÃO DO NOME
 $this->form_validation->set_rules('dnomecompleto', 'Nome completo', 'required|min_length[5]|max_length[40]');
 //VALIDAÇÃO DO EMAIL
-$this->form_validation->set_rules('demail', 'E-mail', 'required|valid_email');
+$this->form_validation->set_rules('demail', 'E-mail', 'required|valid_email|callback_email_check');
+$this->form_validation->set_message('email_check', 'É obrigatório o uso de email com os domínios @soulasalle.com.br ou @lasalle.org.br'); 
 //VALIDAÇÃO DA MATRÍCULA
 $this->form_validation->set_rules('dmatricula', 'Matrícula La Salle', 'required|regex_match[/^[0-9]{10}$/]', 'required|exact_length[10]');
 //VALIDAÇÃO DO LOGRADOURO
-$this->form_validation->set_rules('dlogradouro', 'Logradouro', 'required|min_length[5]|max_length[100]');
+$this->form_validation->set_rules('dlogradouro', 'Logradouro', 'required|min_length[3]|max_length[100]');
 //VALIDAÇÃO DO NUMERO
 $this->form_validation->set_rules('dnumero', 'Número', 'required|min_length[1]|max_length[10]');
 //VALIDAÇÃO DO COMPLEMENTO
-$this->form_validation->set_rules('dcomplemento', 'Complemento', 'required|min_length[5]|max_length[100]');
+$this->form_validation->set_rules('dcomplemento', 'Complemento', 'required|min_length[2]|max_length[100]');
 //VALIDAÇÃO DO CEP
 $this->form_validation->set_rules('dcep', 'CEP', 'required|exact_length[8]');
 //VALIDAÇÃO DO APELIDO
-$this->form_validation->set_rules('dapelido', 'Apelido (username)', 'required|min_length[5]|max_length[15]');
+$this->form_validation->set_rules('dapelido', 'Apelido (username)', 'required|min_length[2]|max_length[15]');
 //VALIDAÇÃO DA SENHA
-$this->form_validation->set_rules('dsenha', 'Senha', 'required|min_length[5]|max_length[100]');
+$this->form_validation->set_rules('dsenha', 'Senha', 'required|min_length[5]|max_length[12]');
 //VALIDAÇÃO DA CONFIRMAÇÃO DE SENHA
-$this->form_validation->set_rules('dconfirmasenha', 'Password Confirmation', 'required|matches[dsenha]|min_length[5]|max_length[12]');
+$this->form_validation->set_rules('dconfirmasenha', 'Confirmação de senha', 'required|matches[dsenha]|min_length[5]|max_length[12]');
 
 if ($this->form_validation->run() == FALSE) {
-
+$data = array('nomecompleto','email', 'matricula','logradouro' ,'numero', 'complemento', 'cep', 'apelido');
                 $this->load->view('index/topo');
                 $this->load->view('index/inicio');
                 $this->load->view('index/oquee');
                 $this->load->view('index/comofunciona');
-                $this->load->view('index/cadastro');
+                $this->load->view('index/cadastro',$data);
                 $this->load->view('index/contato');
                 $this->load->view('index/rodape');
-           
-
-
 } else {
 //Setting values for tabel columns
-
 $criptografado = password_hash($this->input->post('dsenha'), PASSWORD_DEFAULT);
 $data = array(
 'nomecompleto' => $this->input->post('dnomecompleto'),
@@ -59,9 +59,7 @@ $data = array(
 );
 //Transfering data to Model
 $this->insert_model->form_insert($data);
-
 redirect(base_url('sucesso'));
-
 }
 }
 }
--- application/views/index/cadastro.php
+++ application/views/index/cadastro.php
@@ -6,43 +6,41 @@
             <h3 class="section-subheading text-muted">Preencha corretamente os dados abaixo:</h3>
           </div>
         </div>
-
 <?php echo form_open('cadastro'); ?>
 <?php if (isset($message)) { ?>
 <CENTER><h3 style="color:green;">Dados cadastrados com sucesso!</h3></CENTER><br>
 <?php } ?>
-
 <!-- INICIO DOS CAMPOS DO FORMULÁRIO -->
-
 <?php echo form_error('dnomecompleto'); ?>
-<?php echo form_input(array('id' => 'dnomecompleto','placeholder' => 'Nome completo', 'name' => 'dnomecompleto', 'class'=>'form-control')); ?>
-
-<?php echo form_error('demail'); ?><br />
-<?php echo form_input(array('id' => 'demail','placeholder' => 'E-mail', 'name' => 'demail', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dmatricula'); ?><br />
-<?php echo form_input(array('id' => 'dmatricula','placeholder' => 'Matrícula La Salle', 'name' => 'dmatricula', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dlogradouro'); ?><br />
-<?php echo form_input(array('id' => 'dlogradouro','placeholder' => 'Logradouro', 'name' => 'dlogradouro', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dnumero'); ?><br />
-<?php echo form_input(array('id' => 'dnumero','placeholder' => 'Número', 'name' => 'dnumero', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dcomplemento'); ?><br />
-<?php echo form_input(array('id' => 'dcomplemento','placeholder' => 'Complemento', 'name' => 'dcomplemento', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dcep'); ?><br />
-<?php echo form_input(array('id' => 'dcep','placeholder' => 'CEP (somente dígitos)', 'name' => 'dcep', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dapelido'); ?><br />
-<?php echo form_input(array('id' => 'dapelido','placeholder' => 'Apelido (username)', 'name' => 'dapelido', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dsenha'); ?><br />
-<?php echo form_input(array('id' => 'dsenha','placeholder' => 'Senha', 'name' => 'dsenha', 'type' => 'password', 'class'=>'form-control')); ?>
-
-<?php echo form_error('dconfirmasenha'); ?><br />
-<?php echo form_input(array('id' => 'dconfirmasenha','placeholder' => 'Confirme a senha', 'name' => 'dconfirmasenha', 'type' => 'password', 'class'=>'form-control')); ?><br />
+<input type="text" placeholder="Nome completo" class="form-control" name="dnomecompleto" id="dnomecompleto" value="<?php echo set_value('dnomecompleto');?>">
+<br />
+<?php echo form_error('demail'); ?>
+<input type="text" placeholder="E-mail (apenas domínios @soulasalle.com.br e @lasalle.org.br são aceitos)" class="form-control" name="demail" id="demail" value="<?php echo set_value('demail');?>">
+<br />
+<?php echo form_error('dmatricula'); ?>
+<input type="text" placeholder="Matrícula La Salle" class="form-control" name="dmatricula" id="dmatricula" value="<?php echo set_value('dmatricula');?>">
+<br />
+<?php echo form_error('dlogradouro'); ?>
+<input type="text" placeholder="Logradouro" class="form-control" name="dlogradouro" id="dlogradouro" value="<?php echo set_value('dlogradouro');?>">
+<br />
+<?php echo form_error('dnumero'); ?>
+<input type="text" placeholder="Número" class="form-control" name="dnumero" id="dnumero" value="<?php echo set_value('dnumero');?>">
+<br />
+<?php echo form_error('dcomplemento'); ?>
+<input type="text" placeholder="Complemento" class="form-control" name="dcomplemento" id="dcomplemento" value="<?php echo set_value('dcomplemento');?>">
+<br />
+<?php echo form_error('dcep'); ?>
+<input type="text" placeholder="CEP (somente dígitos)" class="form-control" name="dcep" id="dcep" value="<?php echo set_value('dcep');?>">
+<br />
+<?php echo form_error('dapelido'); ?>
+<input type="text" placeholder="Apelido (username)" class="form-control" name="dapelido" id="dapelido" value="<?php echo set_value('dapelido');?>">
+<br />
+<?php echo form_error('dsenha'); ?>
+<input type="text" placeholder="Senha" class="form-control" name="dsenha" id="dsenha">
+<br />
+<?php echo form_error('dconfirmasenha'); ?>
+<input type="text" placeholder="Confirme a senha" class="form-control" name="dconfirmasenha" id="dconfirmasenha">
 <center>
 <?php echo form_submit(array('id' => 'submit', 'value' => 'Cadastrar', 'class'=>'btn btn-primary btn-xl text-uppercase')); ?>
-<?php echo form_close(); ?> <br/></center>
+<?php echo form_close(); ?> <br/>
+</center>
