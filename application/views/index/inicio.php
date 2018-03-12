a1397277216f06e8e838ca2d7d196556c450d26e
Martelotte <leonardomartelotte@gmail.com>
2018-03-11 22:36:32 -0300

    Correção da largura dos campos Apelido e Senha na view inicio.php
    Já testado no Chrome e no Firefox

--- application/views/index/inicio.php
+++ application/views/index/inicio.php
@@ -3,7 +3,6 @@
         <div class="intro-text">
           <div class="intro-lead-in">Seja muito bem vindo ao Anima!</div>
           <div class="intro-heading text-uppercase">JÁ É CADASTRADO?<br>
-
           <!-- BOTÃO DO MODAL 
           <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="" data-toggle="modal" data-target="#login-modal">Login</a>
           FIM DO BOTAO MODAL-->
@@ -14,57 +13,51 @@
           <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true" style="display: none;">
     	  <div class="modal-dialog">
 				<div class="loginmodal-container">
-                                        <br>
-                                        <br>
+              <br>
+              <br>
 					<h1>Acesso do usuário:</h1><br>
 				        <?php  
-                                            //echo validation_errors('<div class="alert alert-danger">','</div>');
-                                            //echo form_open('usuarios/login');
-                                        ?>
+                  //echo validation_errors('<div class="alert alert-danger">','</div>');
+                  //echo form_open('usuarios/login');
+                ?>
                                         
 					<input type="text" name="user" placeholder="Apelido">
-                                        <br>
-                                        <br>
+                <br>
+                <br>
 					<input type="password" name="pass" placeholder="Senha">
-                                        <br>
-                                        <br>
+                <br>
+                <br>
 					<input type="submit" name="login" class="login loginmodal-submit" value="Entrar">
-                                        <button class="btn btn-primary btn-xl text-uppercase">Entrar</button>
-                                        <?php  
-                                         //echo form_close();
-                                        ?>
-                                     
+          <button class="btn btn-primary btn-xl text-uppercase">Entrar</button>
+          <?php  
+          //echo form_close();
+          ?>                           
 				  <div class="login-help">
 					<a href="#">Esqueci minha senha</a>
 				  </div>
 				</div>
 			</div>
 		  </div>
-          
          FIM DO MODAL - VAMOS HABILITÁ-LO DEPOIS DOS TESTES DE LOGIN -->
-
-
           <?php  
                 echo form_error('txt-user');
                 echo form_error('txt-senha');
-
                 //echo validation_errors('<div class="alert alert-danger">','</div>');
                 echo form_open('usuarios/login');
           ?>
-          <fieldset>
-          <div class="form-group">
-					<input class="form-control" type="text" name="txt-user" placeholder="Apelido">
-          </div>
-          <div class="form-group">
-					<input class="form-control" type="password" name="txt-senha" placeholder="Senha">
-          </div>					
-          <button class="btn btn-primary btn-xl text-uppercase js-scroll-trigger">Entrar</button>
-          </fieldset>
+     <center>
+            <div class="form-group col-lg-3">
+                <input style="text-align: center;" class="form-control" type="text" name="txt-user" placeholder="Apelido">
+            </div>
+            <div class="col-lg-3">
+                <input style="text-align: center;" class="form-control col-xs-1" type="password" name="txt-senha" placeholder="Senha">
+            </div>
+          <br>
+           <button class="btn btn-primary btn-xl text-uppercase js-scroll-trigger">Entrar</button>
+				   
           <?php  
           echo form_close();
           ?>
-
           <!-- FIM DO MODAL BOOTSTRAP - LAYER DE LOGIN -->
-
         </div>
     </header>
