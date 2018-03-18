fc406feb2c766931336630e1fc8323cef7835336
Martelotte <leonardomartelotte@gmail.com>
2018-03-18 20:12:32 -0300

    Conserto do menu de navegação
    Agora tem que recolocar o background.

--- application/views/anima/oferece/topo.php
+++ application/views/anima/oferece/topo.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
   <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Anima 1.0 beta</title>
-
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-
     <!-- Custom fonts for this template -->
     <link href="<?php echo base_url('vendor/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
@@ -21,12 +16,21 @@
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
     <!-- Custom styles for this template -->
     <link href="<?php echo base_url('css/agency.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('css/poslogin.css'); ?>" rel='stylesheet' type='text/css'>
+
     <link href='http://overpass-30e2.kxcdn.com/overpass.css' rel='stylesheet' type='text/css'>
     <link href='http://overpass-30e2.kxcdn.com/overpass-mono.css' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url('vendor/jquery/jquery.min.js'); ?>"></script>
+    <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url('vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
+    <!-- Contato form JavaScript -->
+    <script src="<?php echo base_url('js/jqBootstrapValidation.js'); ?>"></script>
+    <script src="<?php echo base_url('js/contato_me.js'); ?>"></script>
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url('js/agency.min.js'); ?>"></script>
   </head>
   <body id="page-top">
-
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
