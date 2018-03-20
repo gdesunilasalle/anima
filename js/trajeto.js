3505f6e499257347ecb4a7be468b3501e0e0fc20
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-20 17:28:39 -0300

    Script de trajeto Otimizado
    O script de trajeto foi otimizado sendo agora bem mais rápido

--- js/trajeto.js
+++ js/trajeto.js
@@ -1,132 +1,85 @@
+var mensagem2 = "";
 
+function endereco(address, casa) {
+    if (address == "Unilasalle") {
+        return "Rua Gastão Gonçalves, 79";
+    }
+    if (address == "Terminal") {
+        return "Terminal Rodoviário Roberto Silveira, Av. Felíciano Sodré, S/N - Centro, Niterói - RJ, 24030-010";
+    }
+    if (address == "Barcas") {
+        return "Praça Arariboia, Praça Araribóia - Centro, Niterói - RJ, 24020-030"
+    }
+    if (address == "mestre") {
+        return "R. Dr. Paulo César, 221"
+    }
+    if (address == casa) {
+        return casa;
 
-function endereco(address,casa){
-if(address=="Unilasalle"){
-  return"Rua Gastão Gonçalves, 79";
-}
-if(address=="Terminal"){
-  return"Terminal Rodoviário Roberto Silveira, Av. Felíciano Sodré, S/N - Centro, Niterói - RJ, 24030-010";
-}
-if(address=="Barcas")
-{
-  return"Praça Arariboia, Praça Araribóia - Centro, Niterói - RJ, 24020-030"
-}
-if(address=="mestre")
-{
-  return "R. Dr. Paulo César, 221"
-}
-if(address==casa){
-return casa;
+    }
 
-}
-
-return "Error";
+    return "Error";
 
 
 }
 
-function loadestimativa(casa)
-{
+function loadestimativa(casa) {
 
-  origem= document.getElementById("select-origem").options[document.getElementById("select-origem").selectedIndex].value;
-  destino = document.getElementById("select-destino").options[document.getElementById("select-destino").selectedIndex].value;
-  transporte=document.getElementById("select-transporte").options[document.getElementById("select-transporte").selectedIndex].value;
-  if(transporte=="A pé")
-  {
-  if(endereco(origem,casa)!="Error"&&endereco(destino,casa)!="Error")
-  {
-  endorigem= endereco(origem,casa);
-  enddestino= endereco(destino,casa);
-  teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?origins="+endorigem+"&mode=walking&destinations="+enddestino+"&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
-  loadJSON(teste, callback);
+    origem = document.getElementById("select-origem").options[document.getElementById("select-origem").selectedIndex].value;
+    destino = document.getElementById("select-destino").options[document.getElementById("select-destino").selectedIndex].value;
+    transporte = document.getElementById("select-transporte").options[document.getElementById("select-transporte").selectedIndex].value;
+    endorigem = endereco(origem, casa);
+    enddestino = endereco(destino, casa);
+    if (endorigem == "Error" || enddestino == "Error") {
+        document.getElementById("estimativas").innerHTML = "";
+        return;
+    }
+    if (transporte == "A pé") {
+        mensagem2 = "<br><br>Opa, notei que a pé demora ";
+        teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + endorigem + "&mode=walking&destinations=" + enddestino + "&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
+        loadJSON(teste, callback);
+        return;
+    }
+
+    if ((transporte == "Carro") || (transporte == "Uber") || (transporte == "Táxi")) {
+
+        mensagem2 = "<br><br>Opa, notei que de carro nesse momento está demorando ";
+        teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + endorigem + "&destinations=" + enddestino + "&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
+        loadJSON(teste, callback);
+        return;
+    }
+
+
+    if (transporte == "bus") {
+        mensagem2 = "<br><br>Opa, notei que de ônibus, nesse momento está demorando ";
+        teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?&departure_time=now&mode=transit&transit_mode=bus&origins=" + endorigem + "&destinations=" + enddestino + "&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
+        loadJSON(teste, callback);
+        return;
+    }
+
+
+    if (transporte == "Bicicleta") {
+            endorigem = endereco(origem, casa);
+            enddestino = endereco(destino, casa);
+            mensagem2 = '<br><br>Opa, notei que de bike nesse momento está demorando ';
+            teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?&mode=bicycling&origins=" + endorigem + "&destinations=" + enddestino + "&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
+            loadJSON(teste, callback);
+            return;
+    }
 }
-else {
 
-    document.getElementById("estimativas").innerHTML = "";
-
-}
-
-}
-
-if((transporte=="Carro")||(transporte=="Uber")||(transporte=="Táxi"))
-{
-if(endereco(origem,casa)!="Error"&&endereco(destino,casa)!="Error")
-{
-endorigem= endereco(origem,casa);
-enddestino= endereco(destino,casa);
-teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?origins="+endorigem+"&destinations="+enddestino+"&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
-loadJSON(teste, callback_car);
-}
-else {
-
-  document.getElementById("estimativas").innerHTML = "";
-
-}
-
-}
-if(transporte=="bus")
-{
-if(endereco(origem,casa)!="Error"&&endereco(destino,casa)!="Error")
-{
-endorigem= endereco(origem,casa);
-enddestino= endereco(destino,casa);
-teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?&departure_time=now&mode=transit&transit_mode=bus&origins="+endorigem+"&destinations="+enddestino+"&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
-loadJSON(teste, callback_bus);
-}
-else {
-
-  document.getElementById("estimativas").innerHTML = "";
+function setup() {
 
 }
 
-}
-
-if(transporte=="Bicicleta")
-{
-if(endereco(origem,casa)!="Error"&&endereco(destino,casa)!="Error")
-{
-endorigem= endereco(origem,casa);
-enddestino= endereco(destino,casa);
-teste = "https://crossorigin.me/https://maps.googleapis.com/maps/api/distancematrix/json?&mode=bicycling&origins="+endorigem+"&destinations="+enddestino+"&key=AIzaSyD7u7OILQGaak0e4TQoCgJHr5oDNxa6hgM";
-loadJSON(teste, callback_bike);
-}
-else {
-
-  document.getElementById("estimativas").innerHTML = "";
-
-}
-
-}
-
-}
-
-function setup(){
-
-}
-
-function callback(data){
-  distancia= data.rows[0].elements[0].distance.text;
+function callback(data) {
+    distancia = data.rows[0].elements[0].distance.text;
     tempo = data.rows[0].elements[0].duration.text;
-  document.getElementById("estimativas").innerHTML = "A distancia da origem até destino é "+distancia+"<br><br>Opa, notei que a pé demora "+tempo;
-}
-function callback_bus(data){
-  distancia= data.rows[0].elements[0].distance.text;
-    tempo = data.rows[0].elements[0].duration.text;
-  document.getElementById("estimativas").innerHTML = "A distancia da origem até destino é "+distancia+"<br><br>Opa, notei que de ônibus ta demorando "+tempo+' nesse momento.';
-}
-function callback_car(data){
-  distancia= data.rows[0].elements[0].distance.text;
-  tempo = data.rows[0].elements[0].duration.text;
-  document.getElementById("estimativas").innerHTML = "A distancia da origem até destino é "+distancia+"<br><br>Opa, notei que de carro nesse momento está demorando "+tempo;
-}
-function callback_bike(data){
-  distancia= data.rows[0].elements[0].distance.text;
-  tempo = data.rows[0].elements[0].duration.text;
-  document.getElementById("estimativas").innerHTML = "A distancia da origem até destino é "+distancia+"<br><br>Opa, notei que de bike nesse momento está demorando "+tempo;
+    let mensagem1 = "A distancia da origem até destino é ";
+    document.getElementById("estimativas").innerHTML = mensagem1 + distancia + mensagem2 + tempo;
 }
 
-
-function draw(){
+function draw() {
 
 
 
