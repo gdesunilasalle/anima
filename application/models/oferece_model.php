0c70bdab08719f32663aee4fde72875004677ad8
Martelotte <leonardomartelotte@gmail.com>
2018-03-22 23:17:59 -0300

    Corrigida a gravação de oferta de caronas
    Já adaptada para a modelagem atual

--- application/models/oferece_model.php
+++ application/models/oferece_model.php
@@ -17,24 +17,25 @@ function buscaLogradouro()
 function criacarona($data)
         { 	$result = $this->db->query("SHOW TABLES LIKE '".$data['usuario']."'");
     		if($result->num_rows >= 1) {
-        	$query = $this->db->query("CREATE TABLE `$data[usuario]`(
-  							`ID` int(11) NOT NULL,
-  							`origem` varchar(200) NOT NULL,
-  							`destino` varchar(200) NOT NULL,
-  							`horario` varchar(200) NOT NULL,
-  							`meiotransporte` varchar(11) NOT NULL,
-  							`usuario` varchar(240) NOT NULL)");
-    		} 
+        	 $query = $this->db->query("CREATE TABLE `$data[usuario]`(
+                `ID` int(11) NOT NULL,
+                `origem` varchar(200) NOT NULL,
+                `destino` varchar(200) NOT NULL,
+                `horario` varchar(200) NOT NULL,
+                `meiotransporte` varchar(11) NOT NULL,
+                `usuario` varchar(240) NOT NULL);");
+          $query = $this->db->query("ALTER TABLE `$data[usuario]` ADD PRIMARY KEY (`ID`)");
+    		} else {
+        // NADA ATRIBUIDO POR ENQUANTO PARA O ELSE//       
+        }
 		}
 function gravacarona($data)
-        {
-
-
-
-        	
-        	$this->db->query("INSERT INTO `$data[usuario]` (
-            	`ID`,`origem`, `destino`, `horario`, `meiotransporte`, `usuario`) 
-            	VALUES (0,'$data[origem]', '$data[destino]', '$data[horario]', '$data[meiotransporte]', '$data[usuario]') 
-          		");  
+        {    $result = $this->db->query("SELECT * FROM `$data[usuario]` WHERE ID = 0 ")->num_rows();
+        if( $result > 0) {
+        $this->db->query("UPDATE `$data[usuario]` SET origem = '$data[origem]', destino = '$data[destino]', horario = '$data[horario]', meiotransporte = '$data[meiotransporte]', usuario = '$data[usuario]' WHERE ID = 0 ");
+} else {
+        $this->db->query("INSERT INTO `$data[usuario]` (`ID`,`origem`, `destino`, `horario`, `meiotransporte`, `usuario`) 
+              VALUES (0,'$data[origem]', '$data[destino]', '$data[horario]', '$data[meiotransporte]', '$data[usuario]') 
+              "); }	 
         }
 }
