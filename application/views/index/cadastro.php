697b9d02f0d6c140c4049226d3cffd4e9cfae6f7
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-03-28 19:28:06 -0300

    Required
    As mensagens de required foram traduzidas

--- application/views/index/cadastro.php
+++ application/views/index/cadastro.php
@@ -10,27 +10,31 @@
 <!-- INICIO DOS CAMPOS DO FORMULÁRIO -->
 <?php echo form_error('dnomecompleto'); ?>
 <font class="required"> Nome Completo</font>
-<input type="text" placeholder="Digite aqui seu nome completo" class="form-control" name="dnomecompleto" id="dnomecompleto" value="<?php echo set_value('dnomecompleto');?>" required>
+<input type="text" placeholder="Digite aqui seu nome completo" class="form-control" name="dnomecompleto" id="dnomecompleto" oninvalid="this.setCustomValidity('Não esqueça de preencher seu nome completo !')"
+onchange="this.setCustomValidity('')" value="<?php echo set_value('dnomecompleto');?>" required>
 <br>
 <font class="required">Email</font> <br>
 <?php echo form_error('demail'); ?>
-<input type="text" placeholder="Digite aqui seu email La Salle (exemplo@soulasalle.com.br ou exemplo@lasalle.org)" class="form-control" name="demail" id="demail" value="<?php echo set_value('demail');?>" required>
+<input type="text" placeholder="Digite aqui seu email La Salle (exemplo@soulasalle.com.br ou exemplo@lasalle.org)" class="form-control" name="demail" id="demail" oninvalid="this.setCustomValidity('Não esqueça de preencher seu email da La Salle !')"
+onchange="this.setCustomValidity('')" value="<?php echo set_value('demail');?>" required>
 <br>
 <font class="required">Matrícula</font> <br>
 <?php echo form_error('dmatricula'); ?>
-<input type="text" placeholder="Digite sua matrícula da La Salle" class="form-control" name="dmatricula" id="dmatricula" maxlength="10" size="10" value="<?php echo set_value('dmatricula');?>" required>
+<input type="text" placeholder="Digite sua matrícula da La Salle" class="form-control" name="dmatricula" id="dmatricula" maxlength="10" size="10" oninvalid="this.setCustomValidity('Não esqueça de preencher a sua matricula da La Salle!')"
+onchange="this.setCustomValidity('')" value="<?php echo set_value('dmatricula');?>" required>
 <br>
 <font class="required">CEP</font> <br>
 <?php echo form_error('dcep'); ?>
-<input type="text" placeholder="Digite aqui o seu CEP" class="form-control" name="dcep" id="dcep" size="10" maxlength="9" onblur="pesquisacep(this.value);" value="<?php echo set_value('dcep');?>" required>
+<input type="text" placeholder="Digite aqui o seu CEP" class="form-control" name="dcep" id="dcep" size="10" maxlength="9" onblur="pesquisacep(this.value);" oninvalid="this.setCustomValidity('Não esqueça de preencher seu CEP !')"
+onchange="this.setCustomValidity('')" value="<?php echo set_value('dcep');?>" required>
 <br>
 <font class="required">Logradouro</font> <br>
 <?php echo form_error('dlogradouro'); ?>
-<input type="text" placeholder="Digite aqui o Logradouro" class="form-control" name="dlogradouro" id="dlogradouro" value="<?php echo set_value('dlogradouro');?>" required>
+<input type="text" placeholder="Digite aqui o Logradouro" class="form-control" name="dlogradouro" id="dlogradouro"  oninvalid="this.setCustomValidity('Não esqueça de preencher o Logradouro!')" onchange="this.setCustomValidity('')" value="<?php echo set_value('dlogradouro');?>" required>
 <br />
 <font class="required">Número</font> <br>
 <?php echo form_error('dnumero'); ?>
-<input type="text" placeholder="Digite aqui o número" class="form-control" name="dnumero" id="dnumero" value="<?php echo set_value('dnumero');?>" required>
+<input type="text" placeholder="Digite aqui o número" class="form-control" name="dnumero" id="dnumero" oninvalid="this.setCustomValidity('Não esqueça de preencher o número !')" onchange="this.setCustomValidity('')" value="<?php echo set_value('dnumero');?>" required>
 <br>
 Complemento <br>
 <?php echo form_error('dcomplemento'); ?>
@@ -38,11 +42,11 @@ Complemento <br>
 <br>
 <font class="required">Senha</font> <br>
 <?php echo form_error('dsenha'); ?>
-<input pattern=".{8,30}" type="password" placeholder="Digite sua senha entre 8 e 30 caracteres" class="form-control" name="dsenha" id="dsenha" required>
+<input pattern=".{8,30}" type="password" placeholder="Digite sua senha entre 8 e 30 caracteres" oninvalid="this.setCustomValidity('Não esqueça de preencher sua senha (Entre 8 e 30 caracteres)!')" onchange="this.setCustomValidity('')" class="form-control" name="dsenha" id="dsenha" required>
 <br>
 <font class="required">Confirme a senha</font> <br>
 <?php echo form_error('dconfirmasenha'); ?>
-<input pattern=".{8,30}" type="password" placeholder="Confirme aqui sua senha" class="form-control" name="dconfirmasenha" id="dconfirmasenha" required>
+<input pattern=".{8,30}" type="password" placeholder="Confirme aqui sua senha" class="form-control" name="dconfirmasenha" oninvalid="this.setCustomValidity('Não esqueça de preencher a confirmação de senha (Entre 8 e 30 caracteres)!')" onchange="this.setCustomValidity('')" id="dconfirmasenha" required>
 <br>
 <center>
 <?php echo form_submit(array('id' => 'submit', 'value' => 'Cadastrar', 'class'=>'btn btn-primary btn-xl text-uppercase')); ?>
