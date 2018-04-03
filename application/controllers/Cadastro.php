64be5b2d73af53f46d3ddaa1a4c7797ff40e0908
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-04-03 13:08:47 -0300

    Correção Cadastro
    Complemento não deve ser obrigatório

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -4,7 +4,7 @@ function __construct() {
 parent::__construct();
 $this->load->model('insert_model');
 }
-public function email_check($email) 
+public function email_check($email)
 {
   return strpos($email, '@soulasalle.com.br') || strpos($email, '@lasalle.org.br') !== false;
 }
@@ -16,7 +16,7 @@ $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 $this->form_validation->set_rules('dnomecompleto', 'Nome completo', 'required|min_length[5]|max_length[40]');
 //VALIDAÇÃO DO EMAIL
 $this->form_validation->set_rules('demail', 'E-mail', 'required|valid_email|callback_email_check');
-$this->form_validation->set_message('email_check', 'É obrigatório o uso de email com os domínios @soulasalle.com.br ou @lasalle.org.br'); 
+$this->form_validation->set_message('email_check', 'É obrigatório o uso de email com os domínios @soulasalle.com.br ou @lasalle.org.br');
 //VALIDAÇÃO DA MATRÍCULA
 $this->form_validation->set_rules('dmatricula', 'Matrícula La Salle', 'required|regex_match[/^[0-9]{10}$/]', 'required|exact_length[10]');
 //VALIDAÇÃO DO LOGRADOURO
@@ -24,7 +24,7 @@ $this->form_validation->set_rules('dlogradouro', 'Logradouro', 'required|min_len
 //VALIDAÇÃO DO NUMERO
 $this->form_validation->set_rules('dnumero', 'Número', 'required|min_length[1]|max_length[10]');
 //VALIDAÇÃO DO COMPLEMENTO
-$this->form_validation->set_rules('dcomplemento', 'Complemento', 'required|min_length[2]|max_length[100]');
+$this->form_validation->set_rules('dcomplemento', 'Complemento', 'min_length[2]|max_length[100]');
 //VALIDAÇÃO DO CEP
 $this->form_validation->set_rules('dcep', 'CEP', 'required|exact_length[8]');
 //VALIDAÇÃO DA SENHA
@@ -58,25 +58,25 @@ $data = array(
 'senha' => $criptografado,
 'hash' => md5(rand(0, 1000)),
 );
- 
+
 //ENVIA EMAIL
       $this->email->from('anima@soulasalle.com.br', 'Anima?!'); //EMAIL DE ORIGEM
       $address = $_POST['demail']; //EMAIL DE DESTINO
       $subject="Bem vindo ao Anima?!";  //TITULO EMAIL
       $message= /*-----------INICIO DO CORPO DO EMAIL-----------*/
-        'Olá, '.$_POST['dnomecompleto'].'! 
+        'Olá, '.$_POST['dnomecompleto'].'!
 
         Obrigado por fazer parte do Anima!
-      
-        Sua conta foi criada com sucesso! 
-        
+
+        Sua conta foi criada com sucesso!
+
         Clique no link de confirmação abaixo para validar o seu cadastro:
-            
-        ' . base_url() . 'index.php/user_registration?' . 
+
+        ' . base_url() . 'index.php/user_registration?' .
         'email=' . $_POST['demail'] . '&hash=' . $data['hash'] ;
 
 
-    /*-----------FIM DO CORPO DO EMAIL-----------*/         
+    /*-----------FIM DO CORPO DO EMAIL-----------*/
       $this->email->to($address);
       $this->email->subject($subject);
       $this->email->message($message);
