3464e7421c3ac598a853dc28627a0c1742d978df
Lucas José <32078930+lucasjose10@users.noreply.github.com>
2018-04-05 18:29:32 -0300

    Update topo.php

--- application/views/index/topo.php
+++ application/views/index/topo.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <link rel="shortcut icon" href="<?php echo base_url('img/favicon.ico'); ?>" type="image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
