b7cd7fce2f7f37bf30b7fd4e76c1229d3370c89a
Leonardo Martelotte <32073964+leonardomartelotte@users.noreply.github.com>
2018-04-05 15:29:41 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:8888/Anima/';
+$config['base_url'] = 'http://www.anima.pe.hu/';
 
 /*
 |--------------------------------------------------------------------------
