bbbc33d8d94fffe666c71ca0a958a59302050960
Martelotte <leonardomartelotte@gmail.com>
2018-04-12 16:19:11 -0300

    FIX para o menu TOPO

--- application/views/anima/edita/topo.php
+++ application/views/anima/edita/topo.php
@@ -1,35 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <link rel="shortcut icon" href="<?php echo base_url('img/favicon.ico'); ?>" type="image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta name="robots" content="noindex">
+    <link rel="shortcut icon" href="favicon.ico"/>
     <title>Anima 1.0 beta</title>
-
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
     <!-- Custom fonts for this template -->
     <link href="<?php echo base_url('vendor/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+<link href='http://overpass-30e2.kxcdn.com/overpass.css' rel='stylesheet' type='text/css'>
+<link href='http://overpass-30e2.kxcdn.com/overpass-mono.css' rel='stylesheet' type='text/css'>
     <!-- Custom styles for this template -->
     <link href="<?php echo base_url('css/agency.css'); ?>" rel="stylesheet">
-    <link href='http://overpass-30e2.kxcdn.com/overpass.css' rel='stylesheet' type='text/css'>
-    <link href='http://overpass-30e2.kxcdn.com/overpass-mono.css' rel='stylesheet' type='text/css'>
-    <!-- Bootstrap core JavaScript -->
-    <script src="<?php echo base_url('vendor/jquery/jquery.min.js'); ?>"></script>
-    <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-    <!-- Plugin JavaScript -->
-    <script src="<?php echo base_url('vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
-    <!-- Contato form JavaScript -->
-    <script src="<?php echo base_url('js/jqBootstrapValidation.js'); ?>"></script>
-    <script src="<?php echo base_url('js/contato_me.js'); ?>"></script>
-    <!-- Custom scripts for this template -->
-    <script src="<?php echo base_url('js/agency.min.js'); ?>"></script>
+ <script src="<?php echo base_url('js/cep.js'); ?>"></script>
   </head>
   <body id="page-top" class="poslogin">
     <!-- Navigation -->
