ef6d8d50d381f9c09bcdd033dcb857fca536c09c
Martelotte <leonardomartelotte@gmail.com>
2018-04-13 14:55:25 -0300

    Correção Painel do Usuário

--- application/controllers/Indice.php
+++ application/controllers/Indice.php
@@ -5,12 +5,10 @@ class Indice extends CI_Controller {
 
 public function __construct() {
         parent::__construct();
-
     }
         public function index()
         { ob_start();
-                if(!$this->session->userdata('logado')){
-               
+                if(!$this->session->userdata('logado')){        
                 $this->load->view('index/topo');
                 $this->load->view('index/inicio');
                 $this->load->view('index/oquee');
@@ -20,19 +18,7 @@ public function __construct() {
                 $this->load->view('index/contato');
                 $this->load->view('index/rodape');
         }else{
-            $email = ($this->session->userdata('email'));
-            $result1  = $this->db->query("SELECT * FROM transportesemcurso WHERE usuario = '$email' AND host = '1' ");
-            $result2  = $this->db->query("SELECT * FROM transportesemcurso WHERE usuario = '$email' AND passageiro != '0' ");
-                if( $result1 > 0) {
-                    $direciona= '2';
-                }
-                    else if( $result2 > 0) {
-                        $direciona= '3';
-                    } 
-                if($direciona=='2' || $direciona=='3'){
-                redirect(base_url('minha'));
-            }else{redirect(base_url('busca'));}
-          
+            redirect(base_url('minha'));
         }
 }
 }
--- application/views/anima/minha/main.php
+++ application/views/anima/minha/main.php
@@ -3,89 +3,69 @@
 <center>
 <section id="cadastro">
       <div class="container">
-<?php if (!$caronas){echo '<h3 class="section-heading text-uppercase sombras">Carona oferecida por mim</h3><h3 class="section-subheading sombras">Você não possui nenhuma oferta ativa de carona no momento</h3>';}?>	
-<?php foreach ($caronas as $info) { ?>
-<h3 class="section-heading text-uppercase sombras">Carona oferecida por mim</h3>
-<h3 class="section-subheading sombras">Aqui você pode editar a carona, removê-la ou conversar com seus colegas pelo chat</h3>
-<?php echo $this->session->flashdata('message');?>
+        <h3 class="section-heading text-uppercase sombras"><u>Meu painel de caronas</u></h3>
+        <br>
+<?php if (!$caronas && !$passageiro){
+echo '<h3 class="section-subheading sombras">Você não está participando de nenhuma carona no momento.<br><br><br>Deseja oferecer uma carona? <a href="'.base_url('oferece').'" class="">Clique aqui!</a><br><br><br>Deseja entrar em uma carona? <a href="'.base_url('busca').'" class="">Clique aqui!</a></h3>';
+}else if($caronas && !$passageiro){
+  echo '<h3 class="section-heading text-uppercase sombras">Carona oferecida por mim</h3>
+<h3 class="section-subheading sombras">Aqui você pode editar a carona, removê-la ou conversar com seus colegas pelo chat</h3>';
+foreach ($caronas as $info) {
+echo $this->session->flashdata('message').'
 <div class="card w-100">
 	  <div class="card-header">
-    <h5 class="text-success"><?php echo $info->emailusuario; ?></h5>
-    <p class="text-secondary"><i><?php echo $info->cursousuario;
+    <h5 class="text-success">'.$info->emailusuario.'</h5>
+    <p class="text-secondary"><i>'.$info->cursousuario;
     if($info->especificacursousuario){echo ' - '.$info->especificacursousuario;}
-    echo '</i></p>';?>
+    echo '</i></p>
   </div>
   <div class="card-body">
-
    		<p class="text-secondary">
-			<?php echo $info->origemusuario; ?>
+			'.$info->origemusuario.'
 			<br>
-			<?php echo $info->destinousuario; ?>
+			'.$info->destinousuario.'
 			<br>
-			<?php echo $info->horariousuario; ?>
+			'.$info->horariousuario.'
 			<br>
-			<?php echo $info->meio; ?></p>
-
-<?php echo form_open('card'); ?>
-<input type="hidden" name="dusuario" id="dusuario" value="<?php echo $info->emailusuario; ?>">
-<?php
-if(($this->session->userdata('email')) == ($info->emailusuario))
-{
-	echo 
-	form_submit(array('id' => 'submit', 'value' => 'Ver detalhes', 'class'=>'btn btn-primary')).
-	'<a href="oferece" class="btn btn-primary">Alterar carona</a>
+			'.$info->meio.'</p>'.form_open('card').'
+<input type="hidden" name="dusuario" id="dusuario" value="'.$info->emailusuario.'">'.
+form_submit(array('id' => 'submit', 'value' => 'Ver detalhes', 'class'=>'btn btn-primary')).
+	'<a href="'.base_url('oferece').'" class="btn btn-primary">Alterar carona</a>
            <a class="btn btn-primary" data-toggle="modal" href="#alertaModal1">
            <div class="comofunciona-hover">
            <div class="comofunciona-hover-content">      
            </div>
            </div>
            Apagar carona
-           </a>';
+           </a>'.form_close();
 }
-else{echo form_submit(array('id' => 'submit', 'value' => 'Animo!', 'class'=>'btn btn-primary'));
-}
-echo form_close();
-?>
-</div>
-</div>
-<br>
-<?php } ?>
-<?php if (!$passageiro){echo '<h3 class="section-heading text-uppercase sombras">Caronas onde sou passageiro</h3><h3 class="section-subheading sombras">Você não possui nenhuma carona ativa no momento</h3>';}?>
-<?php foreach ($passageiro as $info) { ?>
-<h3 class="section-heading text-uppercase sombras">Carona onde sou passageiro</h3>
-<h3 class="section-subheading sombras">Aqui você sair da carona ou conversar com seus colegas pelo chat</h3>
-<?php echo $this->session->flashdata('message');?>
+}else if(!$caronas && $passageiro){
+echo '<h3 class="section-heading text-uppercase sombras">Carona onde sou passageiro</h3>
+<h3 class="section-subheading sombras">Aqui você pode sair da carona ou conversar com seus colegas pelo chat</h3>';
+foreach ($passageiro as $info){
+echo $this->session->flashdata('message').'
 <div class="card w-100">
 	  <div class="card-header">
-    <h5 class="text-success"><?php echo $info->emailusuario; ?></h5>
-    <p class="text-secondary"><i><?php echo $info->cursousuario;
+    <h5 class="text-success">'.$info->emailusuario.'</h5>
+    <p class="text-secondary"><i>'.$info->cursousuario;
     if($info->especificacursousuario){echo ' - '.$info->especificacursousuario;}
-    echo '</i></p>';?>
+    echo '</i></p>'.'
   </div>
   <div class="card-body">
    		<p class="text-secondary">
-			<?php echo $info->origemusuario; ?>
-			<br>
-			<?php echo $info->destinousuario; ?>
-			<br>
-			<?php echo $info->horariousuario; ?>
-			<br>
-			<?php echo $info->meio; ?></p>
-
-<?php echo form_open('card'); ?>
-<input type="hidden" name="dusuario" id="dusuario" value="<?php echo $info->emailusuario; ?>">
-<?php
-if(($this->session->userdata('email')) == ($info->emailusuario))
-{
-	echo '<a href="Apagacarona" class="btn btn-primary">Remover carona</a> <a href="oferece" class="btn btn-primary">Alterar carona</a>';
+      '.$info->origemusuario.'
+      <br>
+      '.$info->destinousuario.'
+      <br>
+      '.$info->horariousuario.'
+      <br>
+      '.$info->meio.'</p>'.form_open('card').'
+<input type="hidden" name="dusuario" id="dusuario" value="'.$info->emailusuario.'">'.form_submit(array('id' => 'submit', 'value' => 'Ver detalhes', 'class'=>'btn btn-primary')).form_close().'
+</div>
+</div>';
 }
-else{echo form_submit(array('id' => 'submit', 'value' => 'Ver detalhes', 'class'=>'btn btn-primary'));
 }
-echo form_close();
 ?>
-</div>
-</div>
-<?php } ?>
 </div>
 </div>
 </header>
