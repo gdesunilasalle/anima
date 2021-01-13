5e7b66aac3333fd3949cbcf9e70e0c6b1d8ade5c
Leonardo Martelotte <leonardomartelotte@gmail.com>
2021-01-13 11:27:14 -0300

    Modo de produção
    Modo de produção

--- application/config/database.php
+++ application/config/database.php
@@ -13,7 +13,7 @@ $db['default'] = array(
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
+	'db_debug' => (ENVIRONMENT == 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
